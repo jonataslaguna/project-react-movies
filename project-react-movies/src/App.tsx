4cb1d119c3a3823e3226ed8d4373c1c4b5a54c30
Jonatas Laguna <jonataslaguna.js@gmail.com>
2023-07-04 11:56:50 -0300

    routes and components config

--- project-react-movies/src/App.tsx
+++ project-react-movies/src/App.tsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+import { Home } from "./components/Home"
+import { Movie } from "./components/Movie"
+import { Search } from "./components/Search"
+
 function App() {
   return (
-    <>
-    <h1>Hello World</h1>
-    </>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="movie/:id" element={<Movie />} />
+        <Route path="search" element={<Search />} />
+      </Routes>
   )
 }
 
---
+++ project-react-movies/src/components/Movie/index.tsx
@@ -0,0 +1,6 @@
+export function Movie() {
+    return (
+        <h1>Movie</h1>
+    )
+}
+
